d110588d42256e9af2a1352a41cdff5b2af2de8f
Yanpeng Wei <wei253@wisc.edu>
2026-07-18 20:24:00 +0800

    .

--- queries/norm_print/3cycle.sql
+++ queries/norm_print/3cycle.sql
@@ -1,6 +1,6 @@
 CREATE VIEW R_1 AS SELECT col0 AS a, col1 AS b FROM R;
 CREATE VIEW R_2 AS SELECT col0 AS b, col1 AS c FROM R;
-CREATE VIEW R_3 AS SELECT col0 AS c, col1 AS a FROM R;
+CREATE VIEW R_3 AS SELECT col0 AS a, col1 AS c FROM R;
 
 -- Lp norms of R_1: degree(a) and degree(b)
 SELECT
@@ -66,5 +66,125 @@ SELECT
   MAX(cnt)                                   AS linf
 FROM (SELECT b, COUNT(*) AS cnt FROM best_ab WHERE tag = 2 GROUP BY b);
 
+-- Baseline Lp norms after ordinary two-way joins, before any split.
+CREATE VIEW r1_join_r2 AS
+  SELECT R_1.a AS a, R_1.b AS b, R_2.c AS c
+  FROM R_1 JOIN R_2 ON R_1.b = R_2.b;
+
+CREATE VIEW r1_join_r3 AS
+  SELECT R_1.a AS a, R_1.b AS b, R_3.c AS c
+  FROM R_1 JOIN R_3 ON R_1.a = R_3.a;
+
+SELECT
+  'R_1 join R_2 degree(a)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT a, COUNT(*) AS cnt FROM r1_join_r2 GROUP BY a)
+UNION ALL
+SELECT
+  'R_1 join R_2 degree(b)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT b, COUNT(*) AS cnt FROM r1_join_r2 GROUP BY b)
+UNION ALL
+SELECT
+  'R_1 join R_2 degree(c)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT c, COUNT(*) AS cnt FROM r1_join_r2 GROUP BY c);
+
+SELECT
+  'R_1 join R_3 degree(a)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT a, COUNT(*) AS cnt FROM r1_join_r3 GROUP BY a)
+UNION ALL
+SELECT
+  'R_1 join R_3 degree(b)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT b, COUNT(*) AS cnt FROM r1_join_r3 GROUP BY b)
+UNION ALL
+SELECT
+  'R_1 join R_3 degree(c)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT c, COUNT(*) AS cnt FROM r1_join_r3 GROUP BY c);
+
+-- Option 2a: Lp norms after split and chosen expansion join, before the closing semijoin.
+CREATE VIEW best_ab_join_r2 AS
+  SELECT best_ab.a AS a, best_ab.b AS b, R_2.c AS c
+  FROM best_ab JOIN R_2 ON best_ab.b = R_2.b
+  WHERE best_ab.tag = 1;
+
+CREATE VIEW best_ab_join_r3 AS
+  SELECT best_ab.a AS a, best_ab.b AS b, R_3.c AS c
+  FROM best_ab JOIN R_3 ON best_ab.a = R_3.a
+  WHERE best_ab.tag = 2;
+
+SELECT
+  'best_ab join R_2 tag=1 degree(a)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT a, COUNT(*) AS cnt FROM best_ab_join_r2 GROUP BY a)
+UNION ALL
+SELECT
+  'best_ab join R_2 tag=1 degree(b)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT b, COUNT(*) AS cnt FROM best_ab_join_r2 GROUP BY b)
+UNION ALL
+SELECT
+  'best_ab join R_2 tag=1 degree(c)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT c, COUNT(*) AS cnt FROM best_ab_join_r2 GROUP BY c);
+
+SELECT
+  'best_ab join R_3 tag=2 degree(a)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT a, COUNT(*) AS cnt FROM best_ab_join_r3 GROUP BY a)
+UNION ALL
+SELECT
+  'best_ab join R_3 tag=2 degree(b)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT b, COUNT(*) AS cnt FROM best_ab_join_r3 GROUP BY b)
+UNION ALL
+SELECT
+  'best_ab join R_3 tag=2 degree(c)' AS grp,
+  COUNT(*)                                   AS l0,
+  SUM(cnt)                                   AS l1,
+  ROUND(POWER(SUM(POWER(cnt, 2)), 1.0/2), 4) AS l2,
+  MAX(cnt)                                   AS linf
+FROM (SELECT c, COUNT(*) AS cnt FROM best_ab_join_r3 GROUP BY c);
+
+DROP VIEW best_ab_join_r3;
+DROP VIEW best_ab_join_r2;
+DROP VIEW r1_join_r3;
+DROP VIEW r1_join_r2;
 
 DROP TABLE best_ab;
